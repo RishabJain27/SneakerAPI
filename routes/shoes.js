4d1e84a858dc82b56aff85780dfd3352440eb5f1
Rishab Jain <rjain9@ucsc.edu>
2020-05-10 22:45:02 -0700

    updating API

--- routes/shoes.js
+++ routes/shoes.js
@@ -1,13 +1,18 @@
 const express = require('express')
 const router = express.Router()
 const Adidas = require('../models/adidas')
+const Nike = require('../models/nike')
+const Underarmour = require('../models/underarmour')
 module.exports = router
 
 //GET All
 router.get('/', async (req, res) => {
     try {
         const adidas = await Adidas.find({})
-        res.json(adidas)
+        const underarmour = await Underarmour.find({})
+        const nike = await Nike.find({})
+        const combine = nike.concat(adidas).concat(underarmour)
+        res.json(combine)
     } catch(err) {
         res.status(500).json({message: err.message})
     }
--- routes/underarmourShoes.js
+++ routes/underarmourShoes.js
@@ -1,13 +1,13 @@
 const express = require('express')
 const router = express.Router()
-const Adidas = require('../models/adidas')
+const Underarmour = require('../models/underarmour')
 module.exports = router
 
 //GET All
 router.get('/', async (req, res) => {
     try {
-        const adidas = await Adidas.find({})
-        res.json(adidas)
+        const subscribers = await Underarmour.find({})
+        res.json(subscribers)
     } catch(err) {
         res.status(500).json({message: err.message})
     }
